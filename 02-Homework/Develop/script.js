04a0905a7d5cd6598af6cdb8608ab455da88391a
Jaswant Jeers <jeerjaswant@yahoo.co.uk>
2022-04-25 18:02:14 +0100

    confirm rules, password criteria function and alert for not selecting any conditions

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -19,39 +19,18 @@ const generatePassword = () => {
   if (expression1 && expression2) {
     //parse entry into an integer
     passwordlengthprompt = parseInt(passwordlengthprompt);
-    // console.log(passwordlengthprompt);
-    // console.log(typeof passwordlengthprompt);
+
+    console.log(passwordlengthprompt);
+    console.log(typeof passwordlengthprompt);
+
     //if entry is not correct alert the user
   } else {
     alert("please enter a valid password length");
     e.default.prevent();
     window.close;
   }
-  //confirm for uppercase (true)
-  //confirm for numeric(true)
-  //confirm for special characters(true)
-  //user will be presented with a confirm for lower case(true)
 
-  var lowercaseprompt = confirm("Do you want lowercase in your password");
-  if (lowercaseprompt) {
-    // console.log("True");
-  }
-  var uppdercaseprompt = confirm("do you want uppercase in your password");
-  if (uppdercaseprompt) {
-    // console.log("True");
-  }
-  var numbercaseprompt = confirm("do you want numbers in your passowrd");
-  if (numbercaseprompt) {
-    // console.log("True");
-  }
-  var specialcharactersprompt = confirm(
-    "do you want special characters in your password"
-  );
-
-  if (specialcharactersprompt) {
-    // console.log("True");
-  }
-
+  //array for lower case letters
   var lowercase = [
     "a",
     "b",
@@ -80,6 +59,7 @@ const generatePassword = () => {
     "y",
     "z",
   ];
+  //array for uppercase letters
 
   const uppercase = [
     "A",
@@ -110,7 +90,10 @@ const generatePassword = () => {
     "Z",
   ];
 
+  //array for numeric
   var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  //array for special characters
   var special = [
     " ",
     "!",
@@ -141,6 +124,60 @@ const generatePassword = () => {
     "}",
     "~",
   ];
+
+  //confirm for uppercase (true)
+  //confirm for numeric(true)
+  //confirm for special characters(true)
+  //user will be presented with a confirm for lower case(true)
+
+  const lowercaseprompt = confirm("Do you want lowercase in your password");
+  if (lowercaseprompt) {
+    console.log("True");
+  }
+  const uppdercaseprompt = confirm("do you want uppercase in your password");
+  if (uppdercaseprompt) {
+    console.log("True");
+  }
+  const numbercaseprompt = confirm("do you want numbers in your passowrd");
+  if (numbercaseprompt) {
+    console.log("True");
+  }
+  const specialcharactersprompt = confirm(
+    "do you want special characters in your password"
+  );
+
+  if (specialcharactersprompt) {
+    console.log("True");
+  }
+
+  //To get an array from the above variables i will be using the following function
+  const getpasswordcriteria = (
+    haslowercase,
+    hasuppercase,
+    hasnumbers,
+    hassymbols
+  ) => {
+    const characterstrue = [
+      ...(haslowercase ? lowercase : []),
+      ...(hasuppercase ? uppercase : []),
+      ...(hasnumbers ? numeric : []),
+      ...(hassymbols ? special : []),
+    ];
+    console.log(characterstrue);
+  };
+
+  getpasswordcriteria(
+    specialcharactersprompt,
+    numbercaseprompt,
+    lowercaseprompt,
+    uppdercaseprompt
+  );
+  if (getpasswordcriteria.length <= 1);
+  {
+    alert("Please select at least 1 condition");
+    e.default.prevent();
+    window.close;
+  }
 
   //RETURN Password should be generated for selected criteria on page
   return "";
