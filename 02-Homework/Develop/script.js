b8bdb0263fb5f643a921e9c1e57b38e3a3ba0a27
Jaswant Jeers <jeerjaswant@yahoo.co.uk>
2022-04-25 21:06:05 +0100

    strings for dataset redifined into strings, did not work as variable. password defined and for loop introduced along with math.floor calculation.

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,11 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//string for lower case letters
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+//string for uppercase letters
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//string for numeric
+var numeric = "0123456789";
+//string for special characters
+var special = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~]";
+
+//starter string
+var charset = "";
+
 //main function to generate the random password
 
 const generatePassword = () => {
-  //Prompt user to enter password length 8-128 characters using PROMPT, COMBINE WITH VAR TO STORE VALUE
-
+  //Prompt user to enter password length 8-128 characters using PROMPT
   var passwordlengthprompt = prompt(
     //MESSAGE TO BE DISPLAYED IN THE CONSOLE
     "Please enter the length of the password you want (8-128) characters",
@@ -20,167 +31,57 @@ const generatePassword = () => {
     //parse entry into an integer
     passwordlengthprompt = parseInt(passwordlengthprompt);
 
-    console.log(passwordlengthprompt);
-    console.log(typeof passwordlengthprompt);
+    // console.log(passwordlengthprompt);
+    // console.log(typeof passwordlengthprompt);
 
-    //if entry is not correct alert the user
+    //if entry is not correct alert the user and close window
   } else {
     alert("please enter a valid password length");
     e.default.prevent();
     window.close;
   }
 
-  //array for lower case letters
-  var lowercase = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  //array for uppercase letters
-
-  const uppercase = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-
-  //array for numeric
-  var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-  //array for special characters
-  var special = [
-    " ",
-    "!",
-    "#",
-    "$",
-    "%",
-    "&",
-    "(",
-    ")",
-    "+",
-    "-",
-    ".",
-    "/",
-    ":",
-    ";",
-    "<",
-    "=",
-    ">",
-    "?",
-    "@",
-    "[",
-    "]",
-    "^",
-    "_",
-    "`",
-    "{",
-    "|",
-    "}",
-    "~",
-  ];
-
-  //confirm for uppercase (true)
-  //confirm for numeric(true)
-  //confirm for special characters(true)
+  //confirm buttons and add to charset
   //user will be presented with a confirm for lower case(true)
-
-  const lowercaseprompt = confirm("Do you want lowercase in your password");
-  if (lowercaseprompt) {
-    console.log("True");
+  const pushlowercase = confirm("Do you want lowercase in your password");
+  if (pushlowercase) {
+    //this will add the string to the starter string
+    charset += lowercase;
+    // console.log("True");
   }
-  const uppdercaseprompt = confirm("do you want uppercase in your password");
-  if (uppdercaseprompt) {
-    console.log("True");
+  //confirm for uppercase (true)
+  const pushuppercase = confirm("do you want uppercase in your password");
+  if (pushuppercase) {
+    //this will add the string to the starter string
+    charset += uppercase;
+    // console.log("True");
   }
-  const numbercaseprompt = confirm("do you want numbers in your passowrd");
-  if (numbercaseprompt) {
-    console.log("True");
+  //confirm for numeric(true)
+  const pushnumeric = confirm("do you want numbers in your passowrd");
+  if (pushnumeric) {
+    //this will add the string to the starter string
+    charset += numeric;
+    // console.log("True");
   }
-  const specialcharactersprompt = confirm(
+  //confirm for special characters(true)
+  const pushspecial = confirm(
     "do you want special characters in your password"
   );
-
-  if (specialcharactersprompt) {
-    console.log("True");
+  if (pushspecial) {
+    //this will add the string to the starter string
+    charset += special;
+    // console.log("True");
   }
 
-  //To get an array from the above variables i will be using the following function
-  const getpasswordcriteria = (
-    haslowercase,
-    hasuppercase,
-    hasnumbers,
-    hassymbols
-  ) => {
-    const characterstrue = [
-      ...(haslowercase ? lowercase : []),
-      ...(hasuppercase ? uppercase : []),
-      ...(hasnumbers ? numeric : []),
-      ...(hassymbols ? special : []),
-    ];
-    console.log(characterstrue);
-  };
+  //declare password as variable
+  var password = "";
 
-  getpasswordcriteria(
-    specialcharactersprompt,
-    numbercaseprompt,
-    lowercaseprompt,
-    uppdercaseprompt
-  );
-  if (getpasswordcriteria.length <= 1);
-  {
-    alert("Please select at least 1 condition");
-    e.default.prevent();
-    window.close;
+  //create a for loop to use the declared password length and use math.floor to randomise the selection
+  for (let i = 0; i < passwordlengthprompt; i++) {
+    password += charset[Math.floor(Math.random() * charset.length)];
   }
-
   //RETURN Password should be generated for selected criteria on page
-  return "";
+  return password;
 };
 
 // Write password to the #password input
