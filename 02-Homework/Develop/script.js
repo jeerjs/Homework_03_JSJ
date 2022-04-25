a30c17fbb697b8940acb43cdfed7d3cba52ff3f3
Jaswant Jeers <jeerjaswant@yahoo.co.uk>
2022-04-25 15:01:03 +0100

    Confirms added for other variables and arrays created

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -2,8 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 //main function to generate the random password
+
 const generatePassword = () => {
-  //Prompt user to enter password length 8-128 characters using PROMPT
+  //Prompt user to enter password length 8-128 characters using PROMPT, COMBINE WITH VAR TO STORE VALUE
+
   var passwordlengthprompt = prompt(
     //MESSAGE TO BE DISPLAYED IN THE CONSOLE
     "Please enter the length of the password you want (8-128) characters",
@@ -13,22 +15,133 @@ const generatePassword = () => {
   var expression1 = passwordlengthprompt >= 8;
   var expression2 = passwordlengthprompt <= 128;
 
-  //IF STATEMENT to determin if entry is true
+  //IF STATEMENT to determine if entry is true
   if (expression1 && expression2) {
     //parse entry into an integer
     passwordlengthprompt = parseInt(passwordlengthprompt);
-    console.log(passwordlengthprompt);
-    console.log(typeof passwordlengthprompt);
+    // console.log(passwordlengthprompt);
+    // console.log(typeof passwordlengthprompt);
     //if entry is not correct alert the user
   } else {
     alert("please enter a valid password length");
+    e.default.prevent();
+    window.close;
   }
-
-  //user will be presented with a confirm for lower case(true)
-
   //confirm for uppercase (true)
   //confirm for numeric(true)
   //confirm for special characters(true)
+  //user will be presented with a confirm for lower case(true)
+
+  var lowercaseprompt = confirm("Do you want lowercase in your password");
+  if (lowercaseprompt) {
+    // console.log("True");
+  }
+  var uppdercaseprompt = confirm("do you want uppercase in your password");
+  if (uppdercaseprompt) {
+    // console.log("True");
+  }
+  var numbercaseprompt = confirm("do you want numbers in your passowrd");
+  if (numbercaseprompt) {
+    // console.log("True");
+  }
+  var specialcharactersprompt = confirm(
+    "do you want special characters in your password"
+  );
+
+  if (specialcharactersprompt) {
+    // console.log("True");
+  }
+
+  var lowercase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  const uppercase = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var special = [
+    " ",
+    "!",
+    "#",
+    "$",
+    "%",
+    "&",
+    "(",
+    ")",
+    "+",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+
   //RETURN Password should be generated for selected criteria on page
   return "";
 };
